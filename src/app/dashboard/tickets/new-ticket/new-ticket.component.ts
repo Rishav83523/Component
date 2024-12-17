0e913408d2d2cd91993b9dd3a426b0ff80a6e7ca
Rishav83523 <rishav.raj@calsoftinc.com>
2024-12-17 10:19:36 +0530

    Html form element

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -10,8 +10,13 @@ import { ControlComponent } from '../../../shared/control/control.component';
   styleUrl: './new-ticket.component.css',
 })
 export class NewTicketComponent {
-  OnSubmit(titleElememt: string, descriptionElememt: string) {
+  OnSubmit(
+    titleElememt: string,
+    descriptionElememt: string,
+    form: HTMLFormElement
+  ) {
     console.log(titleElememt);
     console.log(descriptionElememt);
+    form.reset();
   }
 }
